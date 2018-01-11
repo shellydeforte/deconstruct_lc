db1cbc51374050e63c654455c2bffeafde4221ce
Shelly <shelly.deforte@gmail.com>
2018-01-11 16:56:06 -0500

    Add svm and plotting functions

--- deconstruct_lc/data_stats/len_comp.py
+++ deconstruct_lc/data_stats/len_comp.py
@@ -1,6 +1,7 @@
 import configparser
 import os
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 from deconstruct_lc import tools_fasta
@@ -17,42 +18,64 @@ class LenComp(object):
         self.fd = os.path.join(config['filepaths']['data_fp'])
         self.cb90 = os.path.join(self.fd, 'bc_train_cd90.fasta')
         self.pdb90 = os.path.join(self.fd, 'pdb_train_cd90.fasta')
+        self.pdb_ids, self.pdb_seqs = tools_fasta.fasta_to_id_seq(
+            self.pdb90, minlen=100, maxlen=2000)
+        self.bc_ids, self.bc_seqs = tools_fasta.fasta_to_id_seq(self.cb90,
+                                                                minlen=100,
+                                                                maxlen=2000)
+        self.comp_len_fp = os.path.join(self.fd, 'len_comp', 'len_comp.tsv')
 
-    def get_seqs(self):
-        pdb_seqs = tools_fasta.fasta_to_seq(self.pdb90)
-        pdb_lens = tools_fasta.get_lengths(pdb_seqs)
-        bc_seqs = tools_fasta.fasta_to_seq(self.cb90)
-        bc_lens = tools_fasta.get_lengths(bc_seqs)
-        return pdb_seqs, pdb_lens, bc_seqs, bc_lens
+    def run_svm(self):
+        X, y = self.construct_Xy()
+        #clf = svms.smooth_rbf(X, y)
+        #svms.plot_contour(clf, X, y)
+        clf2 = svms.linear_svc(X, y)
+        svms.plot_contour(clf2, X, y)
 
     def construct_Xy(self):
-        lca_nmo, lce_nmo, lca_pdb, lce_pdb = self.norm_scores()
-        nmo_arr = np.array([lca_nmo, lce_nmo]).T
-        pdb_arr = np.array([lca_pdb, lce_pdb]).T
-        X = np.concatenate((nmo_arr, pdb_arr))
-        y = np.array([0]*len(lca_nmo) + [1]*len(lca_pdb)).T
+        df = pd.read_csv(self.comp_len_fp, sep='\t', index_col=0)
+        y = np.array(df['y']).T
+        aas = 'SGEQAPDTNKRLHVYFIMCW'
+        #cols = ['Length'] + [aa for aa in aas]
+        cols = ['Length']
+        X = np.array(df[cols])
         return X, y
 
     def get_aa_comp(self):
         aas = 'SGEQAPDTNKRLHVYFIMCW'
-        df_dict = {'Protein ID', 'Length'}
-        cols = ['Protein ID', 'Length'] + [aa for aa in aas]
-        print(cols)
-        #pdb_seqs, pdb_lens, bc_seqs, bc_lens = self.get_seqs()
-        #an_pdb_seq = ProteinAnalysis(pdb_seq)
-        #pdb_dict = an_pdb_seq.get_amino_acids_percent()
-        #an_cb_seq = ProteinAnalysis(cb_seq)
-        #cb_dict = an_cb_seq.get_amino_acids_percent()
-        #pdb_bins = []
-        #cb_bins = []
-        #for aa in aas:
-        #    pdb_bins.append(pdb_dict[aa])
-        #    cb_bins.append(cb_dict[aa])
+        cols = ['Protein ID', 'Length', 'y'] + [aa for aa in aas]
+        df_dict = self.init_df(cols)
+        for pdb_seq in self.pdb_seqs:
+            a_pdb_seq = ProteinAnalysis(pdb_seq)
+            pdb_aas = a_pdb_seq.get_amino_acids_percent()
+            for aa in aas:
+                df_dict[aa].append(pdb_aas[aa])
+        for bc_seq in self.bc_seqs:
+            a_bc_seq = ProteinAnalysis(bc_seq)
+            bc_aas = a_bc_seq.get_amino_acids_percent()
+            for aa in aas:
+                df_dict[aa].append(bc_aas[aa])
+        df = pd.DataFrame(df_dict, columns=cols)
+        df.to_csv(self.comp_len_fp, sep='\t')
+
+    def init_df(self, cols):
+        df_dict = {}
+        for col in cols:
+            df_dict[col] = []
+        pdb_lens = tools_fasta.get_lengths(self.pdb_seqs)
+        bc_lens = tools_fasta.get_lengths(self.bc_seqs)
+        pids = self.bc_ids + self.pdb_ids
+        lens = bc_lens + pdb_lens
+        y = [0]*len(self.bc_ids) + [1]*len(self.pdb_ids)
+        df_dict['Protein ID'] = pids
+        df_dict['Length'] = lens
+        df_dict['y'] = y
+        return df_dict
 
 
 def main():
     lc = LenComp()
-    lc.get_aa_comp()
+    lc.run_svm()
 
 
 
--- deconstruct_lc/svm/svms.py
+++ deconstruct_lc/svm/svms.py
@@ -1,20 +1,65 @@
-import os
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.svm import LinearSVC
+
 
 def smooth_rbf(X, y):
-    clf = SVC(C=0.1, cache_size=200, class_weight=None, coef0=0.0,
-              decision_function_shape='ovr', degree=3, gamma='auto',
-              kernel='rbf', max_iter=-1, probability=False, random_state=None,
-              shrinking=True, tol=0.001, verbose=False)
+    clf = SVC(kernel='rbf',
+              C=0.1,
+              cache_size=500,
+              class_weight=None,
+              random_state=0,
+              decision_function_shape='ovr',
+              gamma='auto',
+              max_iter=-1,
+              probability=False,
+              shrinking=True,
+              tol=0.001,
+              verbose=False)
     clf.fit(X, y)
     score = clf.score(X, y)
+    print(score)
     return clf
 
+
 def linear_svc(X, y):
-    clf = LinearSVC(random_state=0)
+    clf = SVC(kernel='linear',
+              C=1,
+              cache_size=500,
+              class_weight=None,
+              random_state=None,
+              decision_function_shape='ovr',
+              gamma='auto',
+              max_iter=-1,
+              probability=False,
+              shrinking=True,
+              tol=.001,
+              verbose=False)
     clf.fit(X, y)
-    return clf.score(X, y)
+    score = clf.score(X, y)
+    print(score)
+    return clf
+
+
+def plot_contour(clf, X, y):
+    plt.scatter(X[:, 0], X[:, 1], c=y, zorder=9, cmap=plt.cm.Paired,
+                edgecolor='black', s=5)
+    x_min = X[:, 0].min()  # x and y are each of the classes
+    x_max = X[:, 0].max()
+    y_min = X[:, 1].min()
+    y_max = X[:, 1].max()
+    # Creates a grid with the same number on each line
+    XX, YY = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
+    # XX, YY are the individual points of the grid.
+    # np.c_[XX.ravel(), YY.ravel()] creates the x,y pairs of the grid (I think)
+    Z = clf.decision_function(np.c_[XX.ravel(), YY.ravel()])
+    # Put the result into a color plot
+    Z = Z.reshape(XX.shape)
+    plt.pcolormesh(XX, YY, Z > 0, cmap=plt.cm.Paired)
+    plt.contour(XX, YY, Z, colors=['white'],
+                linestyles=['-'], linewidth=5, levels=[0], zorder=10)
+    #plt.xlim([100, 500])
+    #plt.ylim([-20, 100])
+    plt.xlabel('LCA score')
+    plt.ylabel('LCE score')
+    plt.show()
