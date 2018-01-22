118c50a73236a6519f5f9426cec2a7617277dda8
Shelly <shelly.deforte@gmail.com>
2018-01-22 15:01:17 -0500

    Delete empty 'run_raw'. Correct write_raw to work with df

--- deconstruct_lc/params/write_raw.py
+++ deconstruct_lc/params/write_raw.py
@@ -2,7 +2,6 @@ import configparser
 import os
 import pandas as pd
 from deconstruct_lc.params import lc_labels
-from deconstruct_lc import tools_fasta
 from deconstruct_lc import tools_lc
 
 config = configparser.ConfigParser()
@@ -11,48 +10,33 @@ cfg_fp = os.path.join(os.path.join(os.path.dirname(__file__), '..',
 config.read_file(open(cfg_fp, 'r'))
 
 class WriteRaw(object):
-    """I NEED TO FIX ALL THIS"""
-    def __init__(self, k):
-        self.train_fp = config['filepaths']['train_fp']
-        self.all_ids, self.all_seqs, self.y = self.get_seqs()
-        self.all_lens = tools_fasta.get_lengths(self.all_seqs)
+    def __init__(self, k, seqs, init_dict):
+        self.seqs = seqs
+        self.init_dict = init_dict
         self.k = k
-
-    def get_seqs(self):
-        pdb_ids, pdb_seqs = tools_fasta.fasta_to_id_seq(self.pdb_fp)
-        cb_ids, cb_seqs = tools_fasta.fasta_to_id_seq(self.bc_fp)
-        all_seqs = cb_seqs + pdb_seqs
-        all_ids = cb_ids + pdb_ids
-        y = [0]*len(cb_ids) + [1]*len(pdb_ids)
-        return all_ids, all_seqs, y
-
 
     def write_lca(self, alph):
         lc_labs = lc_labels.GetLabels(self.k)
-        lcas = lc_labs.create_lcas(alph)
-        df_dict = {}
-        df_dict['Protein ID'] = self.all_ids
-        df_dict['Length'] = self.all_lens
-        df_dict['y'] = self.y
-        for k_lca in lcas:
+        k_lcas = lc_labs.create_lcas(alph)
+        df_dict = self.init_dict
+        for k_lca in k_lcas:
             lca = k_lca.split('_')[1]
-            scores = tools_lc.calc_lca_motifs(self.all_seqs, self.k, lca)
+            scores = tools_lc.calc_lca_motifs(self.seqs, self.k, lca)
             df_dict[k_lca] = scores
-        df = pd.DataFrame(df_dict, columns=['Protein ID', 'Length', 'y']+lcas)
+        cols = ['Protein ID', 'Length', 'y']+k_lcas
+        df = pd.DataFrame(df_dict, columns=cols)
         return df
 
     def write_lce(self):
         lc_labs = lc_labels.GetLabels(self.k)
-        lces = lc_labs.create_lces(self.all_seqs)
-        df_dict = {}
-        df_dict['Protein ID'] = self.all_ids
-        df_dict['Length'] = self.all_lens
-        df_dict['y'] = self.y
-        for k_lce in lces:
+        k_lces = lc_labs.create_lces(self.seqs)
+        df_dict = self.init_dict
+        for k_lce in k_lces:
             lce = float(k_lce.split('_')[1])
-            scores = tools_lc.calc_lce_motifs(self.all_seqs, self.k, lce)
+            scores = tools_lc.calc_lce_motifs(self.seqs, self.k, lce)
             df_dict[k_lce] = scores
-        df = pd.DataFrame(df_dict, columns=['Protein ID', 'Length', 'y']+lces)
+        cols = ['Protein ID', 'Length', 'y']+k_lces
+        df = pd.DataFrame(df_dict, columns=cols)
         return df
 
 
