6bf64946b2042cf4358f29b734514a7c0593f021
Shelly <shelly.deforte@gmail.com>
2018-01-26 12:27:39 -0500

    Change variable names, add print statement

--- deconstruct_lc/data_pdb/pdb_analysis.py
+++ deconstruct_lc/data_pdb/pdb_analysis.py
@@ -1,30 +1,21 @@
-from Bio import SeqIO
-import pandas as pd
 import configparser
-import re
 import os
+import pandas as pd
+
 from deconstruct_lc import tools_fasta
 from deconstruct_lc import tools_lc
-
 
 config = configparser.ConfigParser()
 cfg_fp = os.path.join(os.path.join(os.path.dirname(__file__), '..',
                                    'config.cfg'))
 config.read_file(open(cfg_fp, 'r'))
 
+
 class PdbAnalysis(object):
-    """
-    Need to add secondary structure, and also, need to do normalized score I think.
-    I need to make this point about LC motifs being destablized in the
-    context of the normalized score or the message isn't as powerful.
-    """
     def __init__(self):
         self.pdb_dp = os.path.join(config['filepaths']['data_dp'], 'pdb_prep')
-        self.pdb_miss_fp = os.path.join(self.pdb_dp, 'pdb_norm_all.fasta')
-        self.all_seq = os.path.join(self.pdb_dp, 'all_seqs.fasta')
-        self.all_dis = os.path.join(self.pdb_dp, 'all_dis.fasta')
-        self.pdb_all_df = os.path.join(self.pdb_dp, 'pdb_norm_all.tsv')
-        self.pdb_an_df = os.path.join(self.pdb_dp, 'pdb_analysis.tsv')
+        self.all_fpi = os.path.join(self.pdb_dp, 'pdb_all.tsv')
+        self.an_fpo = os.path.join(self.pdb_dp, 'pdb_analysis.tsv')
         self.k_lca = 6
         self.k_lce = 6
         self.alph_lca = 'SGEQAPDTNKR'
@@ -33,13 +24,15 @@ class PdbAnalysis(object):
         self.lce_label = '{}_{}'.format(self.k_lce, self.thresh_lce)
 
     def write_analysis(self):
-        df = pd.read_csv(self.pdb_all_df, sep='\t')
+        df = pd.read_csv(self.all_fpi, sep='\t')
+        print("Size of dataframe before filtering is {}".format(len(df)))
         df = df.drop_duplicates(subset=['Sequence', 'Missing'], keep=False)
+        print("Size of dataframe after filtering is {}".format(len(df)))
         df = df.reset_index(drop=True)
-        df = self.add_raw_score(df)
-        df.to_csv(self.pdb_an_df, sep='\t')
+        df = self.add_scores(df)
+        df.to_csv(self.an_fpo, sep='\t')
 
-    def add_raw_score(self, df):
+    def add_scores(self, df):
         seqs = list(df['Sequence'])
         miss_seqs = list(df['Missing'])
         lcas = tools_lc.calc_lca_motifs(seqs, self.k_lca, self.alph_lca)
@@ -47,12 +40,12 @@ class PdbAnalysis(object):
         lcs = tools_lc.calc_lc_motifs(seqs, self.k_lca, self.alph_lca,
                                       self.thresh_lce)
         lengths = tools_fasta.get_lengths(seqs)
-        miss = self.get_missing(miss_seqs)
+        miss_count = self.get_missing(miss_seqs)
         df['Length'] = lengths
-        df['Miss Count'] = miss
+        df['Miss Count'] = miss_count
         df[self.lca_label] = lcas
         df[self.lce_label] = lces
-        df['LCA+LCE'] = lcs
+        df['LC'] = lcs
         return df
 
     def get_missing(self, miss_seqs):
@@ -61,55 +54,9 @@ class PdbAnalysis(object):
             miss.append(seq.count('X'))
         return miss
 
-    def get_pids(self):
-        pids, seqs = tools_fasta.fasta_to_id_seq(self.pdb_miss_fp)
-        return pids
-
-    def write_full(self):
-        """Write sequence, missing. Remove histags from both."""
-        all_pids = self.get_pids()
-        count = 0
-        with open(self.all_seq, 'r') as seq_fi, \
-             open(self.all_dis, 'r') as dis_fi:
-            with open(self.pdb_all_df, 'w') as fo:
-                fo.write('Protein ID\tSequence\tMissing\n')
-                for seq_rec, dis_rec in zip(SeqIO.parse(seq_fi, 'fasta'),
-                                        SeqIO.parse(dis_fi, 'fasta')):
-                    pid = tools_fasta.id_cleanup(seq_rec.id)
-                    if pid in all_pids:
-                        count += 1
-                        print(count)
-                        seq, miss_seq = self.remove_histag(str(seq_rec.seq),
-                                                           str(dis_rec.seq))
-                        fo.write('{}\t{}\t{}\n'.format(pid, seq, miss_seq))
-
-    def remove_histag(self, seq, miss_seq):
-        """Remove histags from the sequence and the corresponding missing
-        sequence"""
-        regex = r'H{6}H*'
-        match = re.finditer(regex, seq)
-        indexes = []
-        for item in match:
-            indexes.append(item.start())
-            indexes.append(item.end())
-        nseq = self.slice_seq(indexes, seq)
-        nmseq = self.slice_seq(indexes, miss_seq)
-        return nseq, nmseq
-
-    def slice_seq(self, indexes, seq):
-        if len(indexes) > 0:
-            nseq = seq[:indexes[0]]
-            for i in range(1, len(indexes) - 1, 2):
-                nseq += seq[indexes[i]:indexes[i + 1]]
-            nseq += seq[indexes[-1]:]
-        else:
-            nseq = seq
-        return nseq
-
 
 def main():
     pa = PdbAnalysis()
-    # pa.write_full() # This will take a while
     pa.write_analysis()
 
 
