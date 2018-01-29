d337a182ea1962e28c2613f36dafc2cdc02c0f09
Shelly <shelly.deforte@gmail.com>
2018-01-29 15:48:34 -0500

    Remove pdb_train to tsv

--- deconstruct_lc/data_pdb/fasta_to_tsv.py
+++ deconstruct_lc/data_pdb/fasta_to_tsv.py
@@ -3,7 +3,6 @@ import configparser
 import os
 import pandas as pd
 from deconstruct_lc import tools_fasta
-from deconstruct_lc.data_pdb import histag
 
 
 config = configparser.ConfigParser()
@@ -19,27 +18,16 @@ class FastaTsv(object):
     def __init__(self):
         self.pdb_dp = os.path.join(config['filepaths']['data_dp'], 'pdb_prep')
         self.norm_fpi = os.path.join(self.pdb_dp, 'pdb_norm_cd100.fasta')
-        self.train_fpi = os.path.join(self.pdb_dp, 'pdb_train_cd90.fasta')
         self.all_fpi = os.path.join(self.pdb_dp, 'pdb_all.fasta')
         self.norm_fpo = os.path.join(self.pdb_dp, 'pdb_norm_cd100.tsv')
-        self.train_fpo = os.path.join(self.pdb_dp, 'pdb_train_cd90.tsv')
         self.all_fpo = os.path.join(self.pdb_dp, 'pdb_all.tsv')
         self.all_seq = os.path.join(self.pdb_dp, 'all_seqs.fasta')
         self.all_dis = os.path.join(self.pdb_dp, 'all_dis.fasta')
         self.all_ss = os.path.join(self.pdb_dp, 'all_ss.fasta')
 
     def write_tsv(self):
-        self.train_df()
         self.write_full(self.all_fpi, self.all_fpo)
         self.write_full(self.norm_fpi, self.norm_fpo)
-
-    def train_df(self):
-        pids, seqs = tools_fasta.fasta_to_id_seq(self.train_fpi)
-        lens = tools_fasta.get_lengths(seqs)
-        df_dict = {'Protein ID': pids, 'Sequence': seqs, 'Length': lens}
-        cols = ['Protein ID', 'Sequence', 'Length']
-        df = pd.DataFrame(df_dict, columns=cols)
-        df.to_csv(self.train_fpo, sep='\t')
 
     def df_stats(self):
         norm_df = pd.read_csv(self.norm_fpo, sep='\t', index_col=0)
