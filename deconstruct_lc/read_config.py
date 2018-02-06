0de0d1f83403d500c56f8ab429782278257b04f0
Shelly <shelly.deforte@gmail.com>
2018-02-06 09:48:06 -0500

    Add read test config

--- deconstruct_lc/read_config.py
+++ deconstruct_lc/read_config.py
@@ -10,6 +10,14 @@ def read_config():
     return config
 
 
+def read_test_config():
+    config = configparser.ConfigParser()
+    cfg_fp = os.path.join(os.path.join(os.path.dirname(__file__), '..',
+                                       'config_test.cfg'))
+    config.read_file(open(cfg_fp, 'r'))
+    return config
+
+
 def main():
     pass
 
