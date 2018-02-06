7650705304728dd972de6c97a2b4474a5254cd02
Shelly <shelly.deforte@gmail.com>
2018-02-06 09:39:01 -0500

    Initial commit

---
+++ deconstruct_lc/read_config.py
@@ -0,0 +1,18 @@
+import configparser
+import os
+
+
+def read_config():
+    config = configparser.ConfigParser()
+    cfg_fp = os.path.join(os.path.join(os.path.dirname(__file__), '..',
+                                       'config.cfg'))
+    config.read_file(open(cfg_fp, 'r'))
+    return config
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
