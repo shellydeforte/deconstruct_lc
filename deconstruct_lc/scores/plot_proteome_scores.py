655e0aed8ad22ab3b529522e03e3a745e1748cc8
Shelly <shelly.deforte@gmail.com>
2018-02-13 11:51:18 -0500

    Remove write scores (moved to its own module)

--- deconstruct_lc/scores/plot_proteome_scores.py
+++ deconstruct_lc/scores/plot_proteome_scores.py
@@ -70,42 +70,6 @@ class PlotScores(object):
         plt.show()
 
 
-class WriteScores(object):
-    """
-    Write the normed scores for yeast, human, pdb, bc for plotting
-    """
-    def __init__(self):
-        self.config = read_config.read_config()
-        self.data_dp = self.config['fps']['data_dp']
-        self.train_fp = os.path.join(self.data_dp, 'train.tsv')
-        self.yeast_fp = os.path.join(self.data_dp, 'proteomes',
-                                     'UP000002311_559292_Yeast.fasta')
-        self.human_fp = os.path.join(self.data_dp, 'proteomes',
-                                     'UP000005640_9606_Human.fasta')
-        self.fpo = os.path.join(self.data_dp, 'scores', 'proteomes.tsv')
-
-    def write_scores(self):
-        hum_seqs = tools_fasta.fasta_to_seq(self.human_fp, minlen=100,
-                                            maxlen=2000)
-        yeast_seqs = tools_fasta.fasta_to_seq(self.yeast_fp, minlen=100,
-                                              maxlen=2000)
-        df = pd.read_csv(self.train_fp, sep='\t', index_col=0)
-        bc_seqs = list(df[df['y'] == 0]['Sequence'])
-        pdb_seqs = list(df[df['y'] == 1]['Sequence'])
-        ns = NormScore()
-        hum_scores = ns.lc_norm_score(hum_seqs)
-        yeast_scores = ns.lc_norm_score(yeast_seqs)
-        bc_scores = ns.lc_norm_score(bc_seqs)
-        pdb_scores = ns.lc_norm_score(pdb_seqs)
-        lc_score = hum_scores + yeast_scores + bc_scores + pdb_scores
-        proteome = ['Human']*len(hum_scores) + ['Yeast']*len(yeast_scores) + \
-                   ['BC']*len(bc_scores) + ['PDB']*len(pdb_scores)
-        df_dict = {'Proteome': proteome, 'LC Score': lc_score}
-        cols = ['Proteome', 'LC Score']
-        df = pd.DataFrame(df_dict, columns=cols)
-        df.to_csv(self.fpo, sep='\t')
-
-
 def main():
     #ws = WriteScores()
     #ws.write_scores()
