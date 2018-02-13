78d6ba23fdc5a0c78ba0826c2c93707d4861d453
Shelly <shelly.deforte@gmail.com>
2018-02-13 11:49:38 -0500

    Write file with all individual bcs

--- deconstruct_lc/scores/write_scores.py
+++ deconstruct_lc/scores/write_scores.py
@@ -1,38 +1,65 @@
-import configparser
 import os
 import pandas as pd
 from deconstruct_lc.scores.norm_score import NormScore
-import numpy as np
+from deconstruct_lc.analysis_bc.write_bc_score import BcScore
+from deconstruct_lc import read_config
 
-
-config = configparser.ConfigParser()
-cfg_fp = os.path.join(os.path.join(os.path.dirname(__file__), '..',
-                                   'config.cfg'))
-config.read_file(open(cfg_fp, 'r'))
 
 class WriteNorm(object):
-    """Write tsv file with column header proteome: yeast, human, BC PDB
-    LC score"""
     def __init__(self):
-        data = config['filepaths']['data_dp']
-        self.train_fpi = os.path.join(data, 'train.tsv')
-        self.train_scores = os.path.join(data, 'scores', 'train_scores.tsv')
+        config = read_config.read_config()
+        data = config['fps']['data_dp']
+        prot_dp = os.path.join(data, 'proteomes')
+        self.bc_dp = os.path.join(data, 'bc_analysis')
 
-    def write_train(self):
-        df = pd.read_csv(self.train_fpi, sep='\t', index_col=0)
-        seqs = df['Sequence']
-        y = df['y']
-        ns = NormScore(seqs)
-        scores = ns.lc_norm_score()
-        cols = ['y', 'LC score']
-        df_dict = {'y': y, 'LC score': scores}
+        self.train_fpi = os.path.join(data, 'train.tsv')
+        self.yeast_fp = os.path.join(prot_dp, 'UP000002311_559292_Yeast.fasta')
+        self.human_fp = os.path.join(prot_dp, 'UP000005640_9606_Human.fasta')
+        self.fpo = os.path.join(data, 'scores', 'pdb_bc_scores.tsv')
+
+    def write_scores(self):
+        """Write tsv that is pid, proteome, org, lc score"""
+        pdb_pids, pdb_proteome, pdb_org, pdb_scores = self.get_pdb()
+        bc_pids, bc_proteome, bc_org, bc_scores = self.get_bcs()
+        df_dict = {'Protein ID': pdb_pids + bc_pids,
+                   'Proteome': pdb_proteome + bc_proteome,
+                   'Organism': pdb_org + bc_org,
+                   'LC Score': pdb_scores + bc_scores}
+        cols = ['Protein ID', 'Proteome', 'Organism', 'LC Score']
         df_out = pd.DataFrame(df_dict, columns=cols)
-        df_out.to_csv(self.train_scores, sep='\t')
+        df_out.to_csv(self.fpo, sep='\t')
+
+    def get_pdb(self):
+        df = pd.read_csv(self.train_fpi, sep='\t', index_col=0)
+        pdb_df = df[df['y'] == 1]
+        pids = list(pdb_df['Protein ID'])
+        proteome = ['PDB']*len(pids)
+        org = ['PDB']*len(pids)
+        seqs = list(pdb_df['Sequence'])
+        ns = NormScore()
+        scores = ns.lc_norm_score(seqs)
+        return pids, proteome, org, scores
+
+    def get_bcs(self):
+        bcs = BcScore()
+        bc_names = bcs.get_sheets()
+        pids = []
+        proteome = []
+        org = []
+        scores = []
+        for bc_name in bc_names:
+            bc_fp = os.path.join(self.bc_dp, '{}_score.tsv'.format(bc_name))
+            df = pd.read_csv(bc_fp, sep='\t', index_col=0)
+            pids += list(df['Protein ID'])
+            proteome += [bc_name]*len(list(df['Protein ID']))
+            org += list(df['Organism'])
+            scores += list(df['LC Score'])
+        return pids, proteome, org, scores
 
 
 def main():
     wn = WriteNorm()
-    wn.write_train()
+    wn.write_scores()
 
 
 if __name__ == '__main__':
