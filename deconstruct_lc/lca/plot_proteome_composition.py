333ae5afade34b845ad34f1e3ed81389df326e18
Shelly <shelly.deforte@gmail.com>
2018-02-16 13:12:39 -0500

    Plot boxplot instead of bar chart

--- deconstruct_lc/lca/plot_proteome_composition.py
+++ deconstruct_lc/lca/plot_proteome_composition.py
@@ -3,40 +3,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from deconstruct_lc import read_config
+
+
 class PlotLcProteome():
     """
     Plot the fraction and standard deviation of each amino acid in low
     complexity regions. Data comes from lc_proteome_composition.py
     """
-
     def __init__(self):
-        self.base_dir = os.path.join(os.path.dirname(__file__), '..',
-                                     'data', 'lc_composition', 'data_out')
-        self.fni = os.path.join(self.base_dir, 'lc_composition.tsv')
+        config = read_config.read_config()
+        data_dp = config['fps']['data_dp']
+        self.fpi = os.path.join(data_dp, 'proteomes_analysis', 'lc_composition.tsv')
 
     def plot_lc_comp(self):
-        aas, mean_lcs, stds = self._read_tsv()
-        indexes = list(range(20))
-        indexes = np.array(indexes)
-        fig, ax = plt.subplots()
-        ax.set_title('Fraction of each amino acid in LCRs', size=14)
-        width = 0.5
-        ax.set_xticks(indexes)
-        ax.set_xticklabels(aas, size=14)
-        ax.bar(indexes, mean_lcs, width, color='kkkkkkkkkkkkkkkkkkkk')
-        ax.set_ylim([0, 0.25])
-        for pos, y, err, color in zip(indexes, mean_lcs, stds,
-                                      'kkkkkkkkkkkkkkkkkkkk'):
-            ax.errorbar(pos, y, err, lw=1, capsize=3, capthick=1,
-                        color=color)
+        df = pd.read_csv(self.fpi, sep='\t', index_col=0)
+        medprops, meanprops, whiskerprops, boxprops = self.params()
+        df.plot.box(vert=True,
+                    whis=[5, 95],
+                    widths=0.75,
+                    showfliers=True,
+                    color='black',
+                    patch_artist=False,
+                    showmeans=True,
+                    boxprops=boxprops,
+                    whiskerprops=whiskerprops,
+                    medianprops=medprops,
+                    meanprops=meanprops
+                    )
+        #plt.title('Fraction of each amino acid in LCRs', size=14)
         plt.show()
 
-    def _read_tsv(self):
-        df = pd.read_csv(self.fni, sep='\t', index_col=0)
-        aas = list(df['Amino Acid'])
-        mean_lcs = list(df['Mean'])
-        stds = list(df['Standard Deviation'])
-        return aas, mean_lcs, stds
+    def params(self):
+        medprops = dict(linestyle='-',
+                        color='black')
+        meanprops = dict(marker='o',
+                         markeredgecolor='darkred',
+                         markerfacecolor='darkred',
+                         markersize=4)
+        whiskerprops = dict(color='black',
+                            linestyle='-')
+        boxprops = dict(color='black')
+        return medprops, meanprops, whiskerprops, boxprops
 
 
 def main():
