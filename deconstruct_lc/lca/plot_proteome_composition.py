792450ed1b2667d93aab666df071f4161ed2bb1c
Shelly <shelly.deforte@gmail.com>
2018-02-19 16:18:07 -0500

    Change comments, create fpo for saving fig

--- deconstruct_lc/lca/plot_proteome_composition.py
+++ deconstruct_lc/lca/plot_proteome_composition.py
@@ -1,6 +1,5 @@
 import os
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 from deconstruct_lc import read_config
@@ -8,13 +7,14 @@ from deconstruct_lc import read_config
 
 class PlotLcProteome():
     """
-    Plot the fraction and standard deviation of each amino acid in low
-    complexity regions. Data comes from lc_proteome_composition.py
+    Create boxplots for the fraction of each amino acid between proteomes.
+    Data generated from lca/data_proteome_composition.py
     """
     def __init__(self):
         config = read_config.read_config()
         data_dp = config['fps']['data_dp']
         self.fpi = os.path.join(data_dp, 'proteomes_analysis', 'lc_composition.tsv')
+        self.fig_fpo = os.path.join(data_dp, 'figures', 'lca_comp.png')
 
     def plot_lc_comp(self):
         df = pd.read_csv(self.fpi, sep='\t', index_col=0)
@@ -23,27 +23,25 @@ class PlotLcProteome():
                     whis=[5, 95],
                     widths=0.75,
                     showfliers=True,
-                    color='black',
+                    color='grey',
                     patch_artist=False,
                     showmeans=True,
                     boxprops=boxprops,
                     whiskerprops=whiskerprops,
                     medianprops=medprops,
-                    meanprops=meanprops
-                    )
-        #plt.title('Fraction of each amino acid in LCRs', size=14)
+                    meanprops=meanprops)
         plt.show()
 
     def params(self):
         medprops = dict(linestyle='-',
-                        color='black')
+                        color='grey')
         meanprops = dict(marker='o',
                          markeredgecolor='darkred',
                          markerfacecolor='darkred',
                          markersize=4)
-        whiskerprops = dict(color='black',
+        whiskerprops = dict(color='grey',
                             linestyle='-')
-        boxprops = dict(color='black')
+        boxprops = dict(color='grey')
         return medprops, meanprops, whiskerprops, boxprops
 
 
