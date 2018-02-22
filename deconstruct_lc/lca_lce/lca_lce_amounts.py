d87a3e3a359aef8b932c36d588e60509f53ef680
Shelly <shelly.deforte@gmail.com>
2018-02-22 14:50:36 -0500

    Add SVM calculation

--- deconstruct_lc/lca_lce/lca_lce_amounts.py
+++ deconstruct_lc/lca_lce/lca_lce_amounts.py
@@ -1,7 +1,9 @@
 import os
+import numpy as np
 import pandas as pd
 
 from deconstruct_lc import read_config
+from deconstruct_lc.svm import svms
 from deconstruct_lc import tools_lc
 
 
@@ -19,19 +21,25 @@ class AmountsTrain(object):
         bc_kmers = self.get_counts(bc_seqs)
         pdb_seqs = self.get_seqs(1)
         pdb_kmers = self.get_counts(pdb_seqs)
-        ind = ['LCA & ~LCE', '~LCA & LCE', 'LCA & LCE', 'LCA || LCE', 'Total 6-mers']
-        df_dict = {'LCA/LCE': ind, 'BC 6-mers': bc_kmers, 'PDB 6-mers': pdb_kmers}
-        cols = ['LCA/LCE', 'BC 6-mers', 'PDB 6-mers']
+        bc_tot_kmers = self.count_kmers(bc_seqs)
+        pdb_tot_kmers = self.count_kmers(pdb_seqs)
+        ind = ['LCA', 'LCA & ~LCE', 'LCE', '~LCA & LCE', 'LCA & LCE', 'LCA || LCE']
+        bc_fracs = [item/bc_tot_kmers for item in bc_kmers]
+        pdb_fracs = [item/pdb_tot_kmers for item in pdb_kmers]
+        df_dict = {'LCA/LCE': ind, 'BC 6-mers': bc_kmers, 'BC fracs': bc_fracs,
+                   'PDB 6-mers': pdb_kmers, 'PDB fracs': pdb_fracs}
+        cols = ['LCA/LCE', 'BC 6-mers', 'BC fracs', 'PDB 6-mers', 'PDB fracs']
         df = pd.DataFrame(df_dict, columns=cols)
         print(df)
 
     def get_counts(self, seqs):
-        lca_count = self.count_lca_not_lce(seqs)
-        lce_count = self.count_not_lca_lce(seqs)
-        lca_lce_count = self.count_lca_and_lce(seqs)
-        lc_count = self.count_lca_or_lce(seqs)
-        total_kmers = self.count_kmers(seqs)
-        return [lca_count, lce_count, lca_lce_count, lc_count, total_kmers]
+        lca_count = sum(tools_lc.calc_lca_motifs(seqs, self.k, self.lca))
+        lca_not_lce_count = sum(self.count_lca_not_lce(seqs))
+        lce_count = sum(tools_lc.calc_lce_motifs(seqs, self.k, self.lce))
+        lce_not_lca_count = sum(self.count_not_lca_lce(seqs))
+        lca_lce_count = sum(self.count_lca_and_lce(seqs))
+        lc_count = sum(self.count_lca_or_lce(seqs))
+        return [lca_count, lca_not_lce_count, lce_count, lce_not_lca_count, lca_lce_count, lc_count]
 
     def count_kmers(self, seqs):
         total_kmers = 0
@@ -41,44 +49,75 @@ class AmountsTrain(object):
         return total_kmers
 
     def count_lca_not_lce(self, seqs):
-        total_count = 0
+        all_counts = []
         for seq in seqs:
+            count = 0
             kmers = tools_lc.seq_to_kmers(seq, self.k)
             for kmer in kmers:
                 if tools_lc.lca_motif(kmer, self.lca):
                     if not tools_lc.lce_motif(kmer, self.lce):
-                        total_count += 1
-        return total_count
+                        count += 1
+            all_counts.append(count)
+        return all_counts
 
     def count_not_lca_lce(self, seqs):
-        total_count = 0
+        all_counts = []
         for seq in seqs:
+            count = 0
             kmers = tools_lc.seq_to_kmers(seq, self.k)
             for kmer in kmers:
                 if tools_lc.lce_motif(kmer, self.lce):
                     if not tools_lc.lca_motif(kmer, self.lca):
-                        total_count += 1
-        return total_count
+                        count += 1
+            all_counts.append(count)
+        return all_counts
 
     def count_lca_and_lce(self, seqs):
-        total_count = 0
+        all_counts = []
         for seq in seqs:
+            count = 0
             kmers = tools_lc.seq_to_kmers(seq, self.k)
             for kmer in kmers:
                 if tools_lc.lce_motif(kmer, self.lce):
                     if tools_lc.lca_motif(kmer, self.lca):
-                        total_count += 1
-        return total_count
+                        count += 1
+            all_counts.append(count)
+        return all_counts
 
     def count_lca_or_lce(self, seqs):
         lc_motifs = tools_lc.calc_lc_motifs(seqs, self.k, self.lca, self.lce)
-        return sum(lc_motifs)
+        return lc_motifs
 
     def get_seqs(self, y):
         df = pd.read_csv(self.train_fpi, sep='\t', index_col=0)
         df = df[df['y'] == y]
         seqs = list(df['Sequence'])
         return seqs
+
+    def sum_to_svm(self):
+        df = pd.read_csv(self.train_fpi, sep='\t', index_col=0)
+        seqs = list(df['Sequence'])
+        y = np.array(df['y']).T
+        lca_count = tools_lc.calc_lca_motifs(seqs, self.k, self.lca)
+        lca_not_lce_count = self.count_lca_not_lce(seqs)
+        lce_count = tools_lc.calc_lce_motifs(seqs, self.k, self.lce)
+        lce_not_lca_count = self.count_not_lca_lce(seqs)
+        lca_lce_count = self.count_lca_and_lce(seqs)
+        lc_count = self.count_lca_or_lce(seqs)
+        ind = ['LCA', 'LCA & ~LCE', 'LCE', '~LCA & LCE', 'LCA & LCE',
+               'LCA || LCE']
+        accuracy = [self.run_svm(lca_count, y), self.run_svm(lca_not_lce_count, y),
+                    self.run_svm(lce_count, y), self.run_svm(lce_not_lca_count, y),
+                    self.run_svm(lca_lce_count, y), self.run_svm(lc_count, y)]
+        df_dict = {'LCA/LCE': ind, 'Accuracy': accuracy}
+        cols = ['LCA/LCE', 'Accuracy']
+        df = pd.DataFrame(df_dict, columns=cols)
+        print(df)
+
+    def run_svm(self, motif_sum, y):
+        X = np.array([motif_sum]).T
+        clf = svms.linear_svc(X, y)
+        return clf.score(X, y)
 
 
 def main():
