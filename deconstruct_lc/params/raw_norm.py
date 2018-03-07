76a89510da6cc1b1046f91319b6351f7ec50df01
Shelly <shelly.deforte@gmail.com>
2018-03-07 15:25:43 -0500

    Outline some functions for creating norm files

--- deconstruct_lc/params/raw_norm.py
+++ deconstruct_lc/params/raw_norm.py
@@ -1,17 +1,68 @@
 import os
 import pandas as pd
 
+from deconstruct_lc.len_norm import mb_len_norm
+
 
 class RawNorm(object):
     """
-    Write the normalized scores for the train file for top_svm_lca/lce
+    Read the m/b values for each representative label and write the normalized
+    score
     """
     def __init__(self, config):
         self.config = config
         data_dp = self.config['fps']['data_dp']
         self.param_dp = os.path.join(data_dp, 'params')
+        self.lca_fpi = os.path.join(self.param_dp, 'rep_lca.txt')
+        self.lce_fpi = os.path.join(self.param_dp, 'top_svm_lce.tsv')
+        self.mb_solo_fp = os.path.join(self.param_dp, 'mb_solo.tsv')
+        self.mb_combo_fp = os.path.join(self.param_dp, 'mb_combo.tsv')
         self.k1 = self.config.getint('params', 'k1')
         self.k2 = self.config.getint('params', 'k2')
+
+    def norm_alone(self):
+        """
+        Calculate LCA or LCE only
+        """
+        lca_labs, lce_labs = self.read_labels()
+        raw_scores, lengths, cy, cpids = self.get_raw_score(lca_labs[0], 'lca')
+        for lca_lab in lca_labs:
+            raw_scores, lengths, y, pids = self.get_raw_score(lca_lab, 'lca')
+            assert y == cy
+            assert pids == cpids
+        for lce_lab in lce_labs:
+            raw_scores, lengths, y, pids = self.get_raw_score(lce_lab, 'lce')
+            assert y == cy
+            assert pids == cpids
+
+    def get_raw_score(self, label, type):
+        """Given an lc label, get the corresponding raw scores"""
+        lab_sp = label.split('_')
+        k = lab_sp[0]
+        fpi = os.path.join(self.param_dp, 'raw_{}_{}.tsv'.format(k, type))
+        df_in = pd.read_csv(fpi, sep='\t', index_col=0)
+        raw_scores = list(df_in[label])
+        lengths = list(df_in['Length'])
+        y = list(df_in['y'])
+        pids = list(df_in['Protein ID'])
+        return raw_scores, lengths, y, pids
+
+    def read_labels(self):
+        lce_df = pd.read_csv(self.lce_fpi, sep='\t')
+        lce_labs = list(lce_df['Label'])
+        lca_labs = []
+        with open(self.lca_fpi, 'r') as fpi:
+            for line in fpi:
+                lca_labs.append(line.strip())
+        return lca_labs, lce_labs
+
+    def label_combos(self):
+        lca_labs, lce_labs = self.read_labels()
+        combos = []
+        for lce in lce_labs:
+            for lca in lca_labs:
+                combos.append((lce, lca))
+        return combos
 
     def read_files(self):
         lca_fpi = os.path.join(self.param_dp, 'top_svm_lca.tsv')
@@ -39,15 +90,8 @@ class RawNorm(object):
             df_dict[label] = norm_scores
         return df_dict
 
-    def get_raw_score(self, label, type):
-        """Given an lc label, get the corresponding raw scores"""
-        lab_sp = label.split('_')
-        k = lab_sp[0]
-        fpi = os.path.join(self.param_dp, 'raw_{}_{}.tsv'.format(k, type))
-        df_in = pd.read_csv(fpi, sep='\t', index_col=0)
-        raw_scores = df_in[label]
-        lengths = df_in['Length']
-        return raw_scores, lengths
+    def get_mb(self):
+        pass
 
     @staticmethod
     def norm_function(m, b, raw_scores, lengths):
--- deconstruct_lc/params/run.py
+++ deconstruct_lc/params/run.py
@@ -2,6 +2,7 @@ from deconstruct_lc import read_config
 from deconstruct_lc.params import raw_scores
 from deconstruct_lc.params import raw_svm
 from deconstruct_lc.params import raw_top
+from deconstruct_lc.params import write_mb
 from deconstruct_lc.params import raw_norm
 
 
@@ -19,17 +20,21 @@ class RunRaw(object):
         rs.svm_lca_lce()
 
     def run_rawtop(self):
-        rm = raw_top.RawMb(self.config)
+        rm = raw_top.RawTop(self.config)
         rm.write_top()
+
+    def run_writemb(self):
+        mb = write_mb.WriteMb(self.config)
+        #mb.write_mb_solo()
+        mb.write_mb_combos()
 
     def run_rawnorm(self):
         rn = raw_norm.RawNorm(self.config)
-        rn.read_files()
 
 
 def main():
     rr = RunRaw()
-    rr.run_rawtop()
+    rr.run_writemb()
 
 
 if __name__ == '__main__':
