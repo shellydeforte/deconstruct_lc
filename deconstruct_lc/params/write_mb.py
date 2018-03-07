585f0eb879639de8b5ba4fbb7092aa3dd07382a0
Shelly <shelly.deforte@gmail.com>
2018-03-07 12:22:48 -0500

    Add pearsons, pval, stderr

--- deconstruct_lc/params/write_mb.py
+++ deconstruct_lc/params/write_mb.py
@@ -20,30 +20,27 @@ class WriteMb(object):
         self.k2 = self.config.getint('params', 'k2')
 
     def write_mb_solo(self):
-        lca_labs, lce_labs = self.read_labels()
+        lca_labs, lce_labs = self._read_labels()
         ln = mb_len_norm.LenNorm(self.config)
-        df_dict = {'lc label': [], 'm': [], 'b': []}
+        df_dict = {'lc label': [], 'm': [], 'b': [], 'pearsons': [],
+                   'pval': [], 'stderr': []}
         for lca_lab in lca_labs[0:1]:
             ll = lca_lab.split('_')
             k = int(ll[0])
             lca = str(ll[1])
-            m, b = ln.mb_lca(k, lca)
-            df_dict['lc label'].append(lca_lab)
-            df_dict['m'].append(m)
-            df_dict['b'].append(b)
+            m, b, pearsons, pval, stderr = ln.mb_lca(k, lca)
+            df_dict = self._fill_dict(lca_lab, m, b, pearsons, pval, stderr, df_dict)
         for lce_lab in lce_labs[0:1]:
             ll = lce_lab.split('_')
             k = int(ll[0])
             lce = float(ll[1])
-            m, b = ln.mb_lce(k, lce)
-            df_dict['lc label'].append(lce_lab)
-            df_dict['m'].append(m)
-            df_dict['b'].append(b)
-        cols = ['lc label', 'm', 'b']
+            m, b, pearsons, pval, stderr = ln.mb_lce(k, lce)
+            df_dict = self._fill_dict(lce_lab, m, b, pearsons, pval, stderr, df_dict)
+        cols = ['lc label', 'm', 'b', 'pearsons', 'pval', 'stderr']
         df_out = pd.DataFrame(df_dict, columns=cols)
         df_out.to_csv(self.mb_solo_fp, sep='\t')
 
-    def read_labels(self):
+    def _read_labels(self):
         lce_df = pd.read_csv(self.lce_fpi, sep='\t')
         lce_labs = list(lce_df['Label'])
         lca_labs = []
@@ -51,6 +48,15 @@ class WriteMb(object):
             for line in fpi:
                 lca_labs.append(line.strip())
         return lca_labs, lce_labs
+
+    def _fill_dict(self, label, m, b, pearsons, pval, stderr, df_dict):
+        df_dict['lc label'].append(label)
+        df_dict['m'].append(m)
+        df_dict['b'].append(b)
+        df_dict['pearsons'].append(pearsons)
+        df_dict['pval'].append(pval)
+        df_dict['stderr'].append(stderr)
+        return df_dict
 
     def label_combos(self):
         lca_labs, lce_labs = self.read_labels()
