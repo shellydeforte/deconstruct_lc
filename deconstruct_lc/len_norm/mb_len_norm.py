fd2dbb2c2347277f341ba3b8aa97846a75cfebe3
Shelly <shelly.deforte@gmail.com>
2018-03-07 12:13:16 -0500

    Return the full vector from the linear regression algorithm

--- deconstruct_lc/len_norm/mb_len_norm.py
+++ deconstruct_lc/len_norm/mb_len_norm.py
@@ -8,6 +8,11 @@ from deconstruct_lc import tools_lc
 
 
 class LenNorm(object):
+    """
+    Note that the linear regression algorithm returns the following:
+    m, b, pearsons correlation coefficient, p-value (assuming no slope),
+    standard error of the estimated gradient
+    """
     def __init__(self, config):
         pdb_dp = os.path.join(config['fps']['data_dp'], 'pdb_prep')
         self.pdb_fp = os.path.join(pdb_dp, 'pdb_norm_cd100.tsv')
@@ -23,25 +28,19 @@ class LenNorm(object):
         """LCA"""
         scores = tools_lc.calc_lca_motifs(self.seqs, k, lca)
         lr = linregress(self.lens, scores)
-        m = lr[0]
-        b = lr[1]
-        return m, b
+        return lr
 
     def mb_lce(self, k, lce):
         """LCE"""
         scores = tools_lc.calc_lce_motifs(self.seqs, k, lce)
         lr = linregress(self.lens, scores)
-        m = lr[0]
-        b = lr[1]
-        return m, b
+        return lr
 
     def mb_lc(self, k, lca, lce):
         """LCA || LCE"""
         scores = tools_lc.calc_lc_motifs(self.seqs, k, lca, lce)
         lr = linregress(self.lens, scores)
-        m = lr[0]
-        b = lr[1]
-        return m, b
+        return lr
 
     def mb_lca_and_lce(self, k, lca, lce):
         """LCA & LCE"""
@@ -49,9 +48,7 @@ class LenNorm(object):
         for seq in self.seqs:
             scores.append(tools_lc.count_lca_and_lce(seq, k, lca, lce))
         lr = linregress(self.lens, scores)
-        m = lr[0]
-        b = lr[1]
-        return m, b
+        return lr
 
     def mb_lca_not_lce(self, k, lca, lce):
         """LCA & ~LCE"""
@@ -59,9 +56,7 @@ class LenNorm(object):
         for seq in self.seqs:
             scores.append(tools_lc.count_lca_not_lce(seq, k, lca, lce))
         lr = linregress(self.lens, scores)
-        m = lr[0]
-        b = lr[1]
-        return m, b
+        return lr
 
     def mb_not_lca_lce(self, k, lca, lce):
         """~LCA & LCE"""
@@ -69,9 +64,7 @@ class LenNorm(object):
         for seq in self.seqs:
             scores.append(tools_lc.count_not_lca_lce(seq, k, lca, lce))
         lr = linregress(self.lens, scores)
-        m = lr[0]
-        b = lr[1]
-        return m, b
+        return lr
 
 
 def main():
@@ -80,8 +73,8 @@ def main():
     lca = str(config['score']['lca'])
     lce = float(config['score']['lce'])
     ln = LenNorm(config)
-    m, b = ln.mb_lc(k, lca, lce)
-    print("The slope is {} and the intercept is {}".format(m, b))
+    lr = ln.mb_lc(k, lca, lce)
+    print("The slope is {} and the intercept is {}".format(lr[0], lr[1]))
 
 
 if __name__ == '__main__':
