dea1e001a869e38d65dd7a7ec49b01418d34ca5c
Shelly <shelly.deforte@gmail.com>
2018-03-14 10:18:51 -0400

    Create a method that will remove the lowest ranking features

--- deconstruct_lc/params/ran_forest.py
+++ deconstruct_lc/params/ran_forest.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
+
 class BestFeatures(object):
     def __init__(self, config):
         data_dp = config['fps']['data_dp']
@@ -14,14 +15,22 @@ class BestFeatures(object):
 
     def ran_forest(self):
         X, y, all_labels = self.construct_matrix()
-        clf = RandomForestClassifier(n_estimators=100, random_state=0)
-        clf = clf.fit(X, y)
-        feat_imp = clf.feature_importances_
-        for i, j in zip(feat_imp, all_labels):
-            print('{} {}'.format(i, j))
-        kf = KFold(n_splits=10, random_state=0, shuffle=True)
-        scores = cross_val_score(clf, X, y, cv=kf)
-        print(scores.mean())
+        for i in range(18):
+            clf = RandomForestClassifier(n_estimators=100, random_state=15)
+            clf = clf.fit(X, y)
+            feat_imp = clf.feature_importances_
+            indexes = np.argsort(feat_imp)
+            X = self.remove_feature(indexes, X)
+            kf = KFold(n_splits=3, random_state=0, shuffle=True)
+            scores = cross_val_score(clf, X, y, cv=kf)
+            print(scores.mean())
+            for i in list(indexes):
+                print(all_labels[i])
+            print()
+
+    def remove_feature(self, indexes, X):
+        a = np.delete(X, [indexes[0]], 1)
+        return a
 
     def construct_matrix(self):
         solo_fns = os.listdir(self.solo_dp)
@@ -40,7 +49,6 @@ class BestFeatures(object):
                 norm_scores.append(norm_score)
         for solo_fn in solo_fns:
             full_label = solo_fn[5:-4]
-            print(full_label)
             all_labels.append(full_label)
             fpi = os.path.join(self.solo_dp, solo_fn)
             solo_df = pd.read_csv(fpi, sep='\t', index_col=0)
