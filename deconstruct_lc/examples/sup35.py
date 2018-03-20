738e40285986f35ff8fce836d7ea4f6b077c6780
Shelly <shelly.deforte@gmail.com>
2018-03-20 16:52:47 -0400

    Add method to print scores

--- deconstruct_lc/examples/sup35.py
+++ deconstruct_lc/examples/sup35.py
@@ -4,6 +4,7 @@ import pandas as pd
 from deconstruct_lc import read_config
 from deconstruct_lc import tools_fasta
 from deconstruct_lc.display import display_lc
+from deconstruct_lc.scores.norm_score import NormScore
 
 
 class Sup35(object):
@@ -17,10 +18,20 @@ class Sup35(object):
         disp = display_lc.Display(seq, 'sup35.html')
         disp.write_body()
 
+    def score_by_section(self):
+        seq = tools_fasta.fasta_to_seq(self.sup_fp)[0][0:-1]
+        ns = NormScore()
+        nm_domain = seq[0:253]
+        c_domain = seq[253:]
+        mc_domain = seq[123:]
+        print(ns.lc_norm_score([nm_domain]))
+        print(ns.lc_norm_score([c_domain]))
+        print(ns.lc_norm_score([mc_domain]))
+
 
 def main():
     sup = Sup35()
-    sup.format_seq()
+    sup.score_by_section()
 
 
 if __name__ == '__main__':
