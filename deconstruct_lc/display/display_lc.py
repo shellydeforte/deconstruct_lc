e1726bedfb1a03f06d95d6b148b04d45d53a67b0
Shelly <shelly.deforte@gmail.com>
2018-03-21 11:16:26 -0400

    Read from fasta and include header information

--- deconstruct_lc/display/display_lc.py
+++ deconstruct_lc/display/display_lc.py
@@ -1,31 +1,21 @@
 import os
-import pandas as pd
 
 from deconstruct_lc import read_config
 from deconstruct_lc.scores import norm_score
+from deconstruct_lc import tools_fasta
 from deconstruct_lc import tools_lc
 from deconstruct_lc import tools
 
 class Display(object):
-    """
-    input a sequence, a list of sequences, or a fasta file.
-    Create an html file that will put the LC motifs in bold
-    Create another html file that will highlight charge
-    Create another html file that will highlight Q/N
-    Create another html file that will highlight aromatics
-    Output score
-    1. Just write html page with sequences and score
-    """
-    def __init__(self, seqs, genes, fn_out, color=False):
+    def __init__(self, fasta_in, fn_out, color=False):
         config = read_config.read_config()
         data_dp = config['fps']['data_dp']
         self.k = config['score'].getint('k')
         self.lca = config['score'].get('lca')
         self.lce = config['score'].getfloat('lce')
         self.fp_out = os.path.join(data_dp, 'display', fn_out)
-        self.seqs = seqs
         self.color = color
-        self.genes = genes
+        self.fasta_in = fasta_in
 
     def write_body(self):
         contents = '''
@@ -39,15 +29,20 @@ class Display(object):
         </head>
         <body>
         '''
-        form_seqs = self.form_seq()
+        headers, seqs = tools_fasta.fasta_to_head_seq(self.fasta_in)
+        form_seqs = self.form_seq(seqs)
         ns = norm_score.NormScore()
-        scores = ns.lc_norm_score(self.seqs)
-        sort_scores, sort_form_seqs = tools.sort_list_by_second_list(scores, form_seqs)
-        for seq, score in zip(sort_form_seqs, sort_scores):
+        scores = ns.lc_norm_score(seqs)
+        zip_list = zip(scores, headers, form_seqs)
+        key_fun = lambda pair: pair[0]
+        sorted_tup = sorted(zip_list, reverse=True, key=key_fun)
+        for score, head, seq in sorted_tup:
+            contents += head
+            contents += '<br>'
             contents += seq
             contents += '<br>'
             contents += str(score)
-            contents += '<br>'
+            contents += '<br><br>'
         contents += '''
         </body>
         </html>
@@ -55,9 +50,9 @@ class Display(object):
         with open(self.fp_out, 'w') as fo:
             fo.write(contents)
 
-    def form_seq(self):
+    def form_seq(self, seqs):
         form_seqs = []
-        for seq in self.seqs:
+        for seq in seqs:
             inds = tools_lc.lc_to_indexes(seq, self.k, self.lca, self.lce)
             ranges = list(tools.ints_to_ranges(sorted(list(inds))))
             es = self.format_string(seq, ranges)
@@ -120,9 +115,7 @@ class Display(object):
 
 
 def main():
-    seq = 'MHQQHSKSENKPQQQRKKFEGPKREAILDLAKYKDSKIRVKLMGGKLVIGVLKGYDQLMNLVLDDTVEYMSNPDDENNTELISKNARKLGLTVIRGTILVSLSSAEGSDVLYMQK'
-    dis = Display([seq], 'test.html')
-    dis.write_body()
+    pass
 
 
 if __name__ == '__main__':
