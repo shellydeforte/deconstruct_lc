f2211f0de3617a6440360adae6662950458a4ac6
Shelly <shelly.deforte@gmail.com>
2018-03-21 11:18:31 -0400

    Change name of sort function, and accept zipped lists

--- deconstruct_lc/tools.py
+++ deconstruct_lc/tools.py
@@ -12,23 +12,32 @@ def ints_to_ranges(int_list):
         yield b[0][1], b[-1][1]
 
 
-def sort_list_by_second_list(sort_by, dep_sort, flag=True):
+def sort_lists(zip_list, flag=True):
     """
-    This expression is broken down for learning.
-    lambda defines a function. Given the variable pair, return the 0 indexed value
-    zip combines two lists into a list of tuples
-    The sorted method will accept a list of tuples, and sort by a specific
-    value in each tuple. This is specified by the key.
+    Accepts a list of tuples created by zip(list1, list2, list3...)
+    Will sort by the first item in the tuple
+    lambda defines a function.
+    Given the variable pair, return the 0 indexed value
+    unpack either by for var1, var2, var3 in sorted_tups:
+    or list1, list2, list3 = zip(*sorted_tups)
     """
     key_fun = lambda pair: pair[0]
-    list_tups = zip(sort_by, dep_sort)
-    sorted_tups = sorted(list_tups, reverse=flag, key=key_fun)
-    sorted_by, dep_sorted = zip(*sorted_tups)
-    return list(sorted_by), list(dep_sorted)
+    sorted_tups = sorted(zip_list, reverse=flag, key=key_fun)
+    return sorted_tups
+
+
+def demonstrate_sort():
+    a = [2, 1, 3]
+    b = ['cat', 'the', 'meowed']
+    c = [6, 5, 7]
+    list_tups = zip(a, b, c)
+    key_fun = lambda pair: pair[0]
+    sorted_tups = sorted(list_tups, reverse=True, key=key_fun)
+    return sorted_tups
 
 
 def main():
-    pass
+    demonstrate_sort()
 
 
 if __name__ == '__main__':
