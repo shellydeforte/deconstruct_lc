2a8f25ae58d37df7669dc88943375d5aa9a1ebf5
shellydeforte <shelly.deforte@gmail.com>
2018-04-18 11:34:22 -0400

    Add marcotte aggregation set

--- deconstruct_lc/puncta/puncta_scores.py
+++ deconstruct_lc/puncta/puncta_scores.py
@@ -16,8 +16,69 @@ class PunctaScores(object):
     def __init__(self):
         config = read_config.read_config()
         data_dp = config['fps']['data_dp']
-        self.puncta_fp = os.path.join(data_dp, 'puncta', 'puncta_proteins.xlsx')
+        self.puncta_fp = os.path.join(data_dp, 'puncta', 'marcotte', 'puncta_proteins.xlsx')
         self.orf_trans = os.path.join(data_dp, 'proteomes', 'orf_trans.fasta')
+        self.agg_fp = os.path.join(data_dp, 'puncta', 'oconnel_agg_list')
+
+    def agg_vs_puncta(self):
+        ns = NormScore()
+        labels = ['Foci (180)', 'Aggregates (117)', 'Foci-Aggregates (158)',
+                  'Aggregates-Foci (95)', 'Aggregates&Foci (22)']
+        agg_orfs = set(self.read_agg())
+        puncta_orfs = set(self.get_ids('ST1'))
+        agg = ns.lc_norm_score(self.get_seqs(agg_orfs))
+        puncta = ns.lc_norm_score(self.get_seqs(puncta_orfs))
+        puncta_agg = ns.lc_norm_score(self.get_seqs(puncta_orfs - agg_orfs))
+        agg_puncta = ns.lc_norm_score(self.get_seqs(agg_orfs - puncta_orfs))
+        puncta_and_agg = ns.lc_norm_score(self.get_seqs(puncta_orfs&agg_orfs))
+        all_scores = [puncta, agg, puncta_agg, agg_puncta, puncta_and_agg]
+        self.matplot_box_plots(all_scores, labels)
+
+    def matplot_box_plots(self, scores, labs):
+        """
+        For doing background:
+        https://stackoverflow.com/questions/18215276/how-to-fill-rainbow-color-under-a-curve-in-python-matplotlib
+        """
+        fig = plt.figure(figsize=(7.5, 3))
+        ax = fig.add_subplot(111)
+        ax.add_patch(patches.Rectangle((-30, 0), 30, 6, facecolor='grey'))
+        ax.add_patch(patches.Rectangle((0, 0), 20, 6, facecolor='darkgrey'))
+        ax.add_patch(patches.Rectangle((20, 0), 100, 6, facecolor='white'))
+        ax.set_xlim([-30 ,110])
+        ax.set_ylim([0, 4])
+        bp = {'color': 'black'}
+        wp = {'color': 'black', 'linestyle':'-'}
+        meanprops = dict(marker='o',
+                              markeredgecolor='black',
+                              markerfacecolor='black',
+                              markersize=3)
+        medianprops = dict(linestyle='-', color='black')
+        ax.boxplot(scores,
+                   vert=False,
+                   whis=[5, 95],
+                   widths=0.5,
+                   labels=labs,
+                   showmeans=True,
+                   showfliers=False,
+                   boxprops=bp,
+                   whiskerprops=wp,
+                   meanprops=meanprops,
+                   medianprops=medianprops)
+        plt.xticks(np.arange(-30, 111, 10))
+        plt.xlabel('LC score')
+        plt.tick_params(axis='both', left='on', top='on', right='on',
+                        bottom='on', labelleft='off', labeltop='off',
+                        labelright='on', labelbottom='on')
+        plt.tight_layout()
+        plt.show()
+
+    def read_agg(self):
+        orfs = []
+        with open(self.agg_fp, 'r') as fi:
+            for line in fi:
+                orf = line.strip()
+                orfs.append(orf)
+        return orfs
 
     def run_plot(self):
         #st3_scores = self.insol_remove_puncta()
@@ -118,44 +179,6 @@ class PunctaScores(object):
         scores = ns.lc_norm_score(seqs)
         return scores
 
-    def matplot_box_plots(self, scores, labs):
-        """
-        For doing background:
-        https://stackoverflow.com/questions/18215276/how-to-fill-rainbow-color-under-a-curve-in-python-matplotlib
-        """
-        fig = plt.figure(figsize=(7.5, 3))
-        ax = fig.add_subplot(111)
-        ax.add_patch(patches.Rectangle((-30, 0), 30, 6, facecolor='grey'))
-        ax.add_patch(patches.Rectangle((0, 0), 20, 6, facecolor='darkgrey'))
-        ax.add_patch(patches.Rectangle((20, 0), 100, 6, facecolor='white'))
-        ax.set_xlim([-30 ,110])
-        ax.set_ylim([0, 4])
-        bp = {'color': 'black'}
-        wp = {'color': 'black', 'linestyle':'-'}
-        meanprops = dict(marker='o',
-                              markeredgecolor='black',
-                              markerfacecolor='black',
-                              markersize=3)
-        medianprops = dict(linestyle='-', color='black')
-        ax.boxplot(scores,
-                   vert=False,
-                   whis=[5, 95],
-                   widths=0.5,
-                   labels=labs,
-                   showmeans=True,
-                   showfliers=False,
-                   boxprops=bp,
-                   whiskerprops=wp,
-                   meanprops=meanprops,
-                   medianprops=medianprops)
-        plt.xticks(np.arange(-30, 111, 10))
-        plt.xlabel('LC score')
-        plt.tick_params(axis='both', left='on', top='on', right='on',
-                        bottom='on', labelleft='off', labeltop='off',
-                        labelright='on', labelbottom='on')
-        plt.tight_layout()
-        plt.show()
-
     def get_scores(self, sn):
         orf_ids = self.get_ids(sn)
         seqs = self.get_seqs(orf_ids)
@@ -168,6 +191,11 @@ class PunctaScores(object):
         orf_ids = list(df['ORF'])
         return orf_ids
 
+    def calc_scores(self, seqs):
+        ns = NormScore()
+        scores = ns.lc_norm_score(seqs)
+        return scores
+
     def get_seqs(self, orf_ids):
         seqs = tools_fasta.get_yeast_seq_from_ids(self.orf_trans, orf_ids)
         return seqs
@@ -175,7 +203,7 @@ class PunctaScores(object):
 
 def main():
     ps = PunctaScores()
-    ps.cont_table_power()
+    ps.agg_vs_puncta()
 
 
 if __name__ == '__main__':
