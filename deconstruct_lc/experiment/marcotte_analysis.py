c6599eee9af016b44d49ed11b68c4855faa874ad
Shelly <shelly.deforte@gmail.com>
2018-08-01 14:40:13 -0400

    puncta vs. nopuncta chi2, length, histograms

--- deconstruct_lc/experiment/marcotte_analysis.py
+++ deconstruct_lc/experiment/marcotte_analysis.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.stats import chi2_contingency
 
 from deconstruct_lc import read_config
@@ -12,22 +13,28 @@ class MarcotteAnalysis(object):
     def __init__(self):
         config = read_config.read_config()
         data_dp = os.path.join(config['fps']['data_dp'])
-        self.marcotte = os.path.join(data_dp, 'experiment', 'marcotte_puncta_scores.tsv')
-        self.huh = os.path.join(data_dp, 'experiment', 'huh_scores.tsv')
+        self.puncta = os.path.join(data_dp, 'experiment', 'marcotte_puncta_scores.tsv')
+        self.nopuncta = os.path.join(data_dp, 'experiment', 'marcotte_nopuncta_scores.tsv')
 
     def read_files(self):
         """
         All marcotte proteins are >= 100 and <= 2000
         862/886 meet this condition for Huh proteins
         """
-        marc_df = pd.read_csv(self.marcotte, sep='\t')
-        marc_df = marc_df[(marc_df['Length'] >= 100) & (marc_df['Length'] <= 2000)]
-        huh_df = pd.read_csv(self.huh, sep='\t')
-        huh_df = huh_df[(huh_df['Length'] >= 100) & (huh_df['Length'] <= 2000)]
-        huh_df = huh_df[~huh_df['ORF'].isin(list(marc_df['ORF']))]
-        print(len(huh_df))
-        mlt, mm, mgt = self.get_bins(marc_df)
-        hlt, hm, hgt = self.get_bins(huh_df)
+        puncta_df = pd.read_csv(self.puncta, sep='\t')
+        puncta_short = puncta_df[puncta_df['Length'] <= 250]
+        puncta_df = puncta_df[(puncta_df['Length'] >= 100) & (puncta_df['Length'] <= 2000)]
+        nopuncta_df = pd.read_csv(self.nopuncta, sep='\t')
+        nopuncta_df = nopuncta_df[(nopuncta_df['Length'] >= 100) & (nopuncta_df['Length'] <= 2000)]
+        nopuncta_short = nopuncta_df[nopuncta_df['Length'] < 250]
+        nopuncta_df['LC Score'].hist(bins=30, range=(-60, 250), normed=True)
+        puncta_df['LC Score'].hist(bins=30, range=(-60, 250), normed=True, alpha=0.5)
+        plt.show()
+        nopuncta_df['Length'].hist(bins=30, range=(100, 2000), normed=True)
+        puncta_df['Length'].hist(bins=30, range=(100, 2000), normed=True, alpha=0.5)
+        plt.show()
+        mlt, mm, mgt = self.get_bins(puncta_df)
+        hlt, hm, hgt = self.get_bins(nopuncta_df)
         cont = np.array([[mlt, mm, mgt], [hlt, hm, hgt]])
         print(cont)
         p = chi2_contingency(cont)[1]
