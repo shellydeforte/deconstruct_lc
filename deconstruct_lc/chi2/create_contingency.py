7bc46cd4a5bca6f940b4192686bd5df40a32dd6a
Shelly <shelly.deforte@gmail.com>
2018-08-03 16:20:31 -0400

    Remove unused imports

--- deconstruct_lc/chi2/create_contingency.py
+++ deconstruct_lc/chi2/create_contingency.py
@@ -1,10 +1,6 @@
 import os
-import numpy as np
 import pandas as pd
-from scipy.stats import chi2_contingency
-from itertools import combinations
 
-from deconstruct_lc.analysis_bc.write_bc_score import BcScore
 from deconstruct_lc import read_config
 
 
