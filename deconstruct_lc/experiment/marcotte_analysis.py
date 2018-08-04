7f25f098af86902fd13a2440c0325b7129245691
Shelly <shelly.deforte@gmail.com>
2018-08-04 10:18:08 -0400

    Remove unused lines

--- deconstruct_lc/experiment/marcotte_analysis.py
+++ deconstruct_lc/experiment/marcotte_analysis.py
@@ -22,11 +22,9 @@ class MarcotteAnalysis(object):
         862/886 meet this condition for Huh proteins
         """
         puncta_df = pd.read_csv(self.puncta, sep='\t')
-        puncta_short = puncta_df[puncta_df['Length'] <= 250]
         puncta_df = puncta_df[(puncta_df['Length'] >= 100) & (puncta_df['Length'] <= 2000)]
         nopuncta_df = pd.read_csv(self.nopuncta, sep='\t')
         nopuncta_df = nopuncta_df[(nopuncta_df['Length'] >= 100) & (nopuncta_df['Length'] <= 2000)]
-        nopuncta_short = nopuncta_df[nopuncta_df['Length'] < 250]
         nopuncta_df['LC Score'].hist(bins=30, range=(-60, 250), normed=True)
         puncta_df['LC Score'].hist(bins=30, range=(-60, 250), normed=True, alpha=0.5)
         plt.show()
