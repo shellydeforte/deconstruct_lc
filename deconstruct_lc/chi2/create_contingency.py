e5400e072840744fa7805bb75bb50133085007d6
Shelly <shelly.deforte@gmail.com>
2018-08-04 09:58:01 -0400

    Add writing functions to chi2 and remove old chi2 script

--- deconstruct_lc/chi2/create_contingency.py
+++ deconstruct_lc/chi2/create_contingency.py
@@ -21,6 +21,7 @@ class BcProteome(object):
         prot_df = self.get_proteome()
         bc_counts = get_bins(bc_df)
         prot_counts = get_bins(prot_df)
+        print('Returning low, med, high of bc, followed by proteome background')
         return bc_counts, prot_counts
 
     def get_bc_ids(self):
@@ -47,6 +48,27 @@ class BcProteome(object):
         df = df[(df['Organism'] == self.organism.upper()) & (df['Proteome'] == self.bc)]
         return df
 
+    def get_bc_cats(self):
+        df = pd.read_csv(self.fpi, sep='\t', index_col=0)
+        bcs = list(set(df['Proteome']))
+        print(bcs)
+
+
+class Marcotte(object):
+    def __init__(self):
+        config = read_config.read_config()
+        data_dp = config['fps']['data_dp']
+        self.puncta = os.path.join(data_dp, 'experiment', 'marcotte_puncta_scores.tsv')
+        self.nopuncta = os.path.join(data_dp, 'experiment', 'marcotte_nopuncta_scores.tsv')
+
+    def get_cont_table(self):
+        puncta_df = pd.read_csv(self.puncta, sep='\t', index_col=0)
+        nopuncta_df = pd.read_csv(self.nopuncta, sep='\t', index_col=0)
+        puncta_counts = get_bins(puncta_df)
+        nopuncta_counts = get_bins(nopuncta_df)
+        print('Returning low, med, high of puncta, followed by nopuncta')
+        return puncta_counts, nopuncta_counts
+
 
 def get_bins(df):
     ndf = df[df['LC Score'] < 0]
@@ -60,11 +82,15 @@ def get_bins(df):
 
 
 def main():
-    bc = BcProteome('P_Body', 'Human')
+    m = Marcotte()
+    puncta, nopuncta = m.get_cont_table()
+    print(puncta)
+    print(nopuncta)
+    bc = BcProteome('Nucleolus', 'Yeast')
     bc_counts, prot_counts = bc.get_cont_table()
     print(bc_counts)
     print(prot_counts)
-
+    bc.get_bc_cats()
 
 
 if __name__ == '__main__':
--- deconstruct_lc/chi2/run_chi2.py
+++ deconstruct_lc/chi2/run_chi2.py
@@ -4,5 +4,84 @@ import pandas as pd
 from scipy.stats import chi2_contingency
 from itertools import combinations
 
-from deconstruct_lc.analysis_bc.write_bc_score import BcScore
 from deconstruct_lc import read_config
+from deconstruct_lc.chi2 import create_contingency
+
+
+def format_cont(row1, row2):
+    return np.array([list(row1), list(row2)])
+
+
+def write_bc_vs_bg(bcs, fpo, organism):
+    adict = {'BC': [], 'pval': []}
+    for bc in bcs:
+        bcc = create_contingency.BcProteome(bc, organism)
+        bc_cont, org_cont = bcc.get_cont_table()
+        if sum(list(bc_cont)) > 10:
+            ct = format_cont(bc_cont, org_cont)
+            pval = chi2_contingency(ct)[1]
+            adict['BC'].append(bc)
+            adict['pval'].append(pval)
+    df_out = pd.DataFrame(adict, columns=['BC', 'pval'])
+    df_out.to_csv(fpo, sep='\t')
+
+
+def run_bc_vs_bg():
+    bcs = ['Nuclear_Stress_Granule', 'Nucleolus', 'P_granule', 'PDB',
+           'PML_Body', 'P_Body', 'Nuclear_Speckles', 'Cytoplasmic_Stress_Granule',
+           'Cajal_bodies', 'Paraspeckle', 'Centrosome']
+    # Compare BC to background
+    config = read_config.read_config()
+    data_dp = config['fps']['data_dp']
+    yeast_fpo = os.path.join(data_dp, 'chi2', 'bc_vs_yeast.tsv')
+    human_fpo = os.path.join(data_dp, 'chi2', 'bc_vs_human.tsv')
+    write_bc_vs_bg(bcs, yeast_fpo, 'Yeast')
+    write_bc_vs_bg(bcs, human_fpo, 'Human')
+
+
+def write_bc_vs_bc(bcs, fpo, organism):
+    combs = list(combinations(bcs, 2))
+    adict = {'BC': [], 'pval': []}
+    for comb in combs:
+        bc1 = comb[0]
+        bc2 = comb[1]
+        bcc1 = create_contingency.BcProteome(bc1, organism)
+        bc1_cont, org_cont = bcc1.get_cont_table()
+        bcc2 = create_contingency.BcProteome(bc2, organism)
+        bc2_cont, org_cont = bcc2.get_cont_table()
+        if sum(list(bc1_cont)) > 10 and sum(list(bc2_cont)) > 10:
+            ct = format_cont(bc1_cont, bc2_cont)
+            pval = chi2_contingency(ct)[1]
+            adict['BC'].append(comb)
+            adict['pval'].append(pval)
+    df_out = pd.DataFrame(adict, columns=['BC', 'pval'])
+    df_out.to_csv(fpo, sep='\t')
+
+
+def run_bc_vs_bc():
+    bcs = ['Nuclear_Stress_Granule', 'Nucleolus', 'P_granule', 'PDB',
+           'PML_Body', 'P_Body', 'Nuclear_Speckles', 'Cytoplasmic_Stress_Granule',
+           'Cajal_bodies', 'Paraspeckle', 'Centrosome']
+    # Compare BC to each other
+    config = read_config.read_config()
+    data_dp = config['fps']['data_dp']
+    yeast_fpo = os.path.join(data_dp, 'chi2', 'bc_vs_bc_yeast.tsv')
+    human_fpo = os.path.join(data_dp, 'chi2', 'bc_vs_bc_human.tsv')
+    write_bc_vs_bc(bcs, yeast_fpo, 'Yeast')
+    write_bc_vs_bc(bcs, human_fpo, 'Human')
+
+
+def main():
+    bcs = ['Nuclear_Stress_Granule', 'Nucleolus', 'P_granule', 'PDB',
+           'PML_Body', 'P_Body', 'Nuclear_Speckles', 'Cytoplasmic_Stress_Granule',
+           'Cajal_bodies', 'Paraspeckle', 'Centrosome']
+    # Compare BC to each other
+    run_bc_vs_bc()
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
