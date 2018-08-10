f4c3e41243d0f6a76abe08af5f5eae9774b90e8c
Shelly <shelly.deforte@gmail.com>
2018-08-10 13:07:42 -0400

    Initial Commit

---
+++ deconstruct_lc/remove_structure/read_pfam.py
@@ -0,0 +1,61 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from deconstruct_lc import read_config
+
+class Pfam(object):
+    def __init__(self):
+        config = read_config.read_config()
+        data_dp = os.path.join(config['fps']['data_dp'])
+        self.pfam_fp = os.path.join(data_dp, 'pfam', 'Pfam-A.regions.uniprot.tsv')
+        self.sgd_uni_fp = os.path.join(data_dp, 'proteomes', 'yeast_pd.xlsx')
+        self.puncta = os.path.join(data_dp, 'experiment', 'marcotte_puncta_proteins.xlsx')
+        self.puncta_map = os.path.join(data_dp, 'experiment', 'puncta_map.xlsx')
+        self.nopuncta_map = os.path.join(data_dp, 'experiment', 'nopuncta_map.tsv')
+        self.nopuncta_map_excel = os.path.join(data_dp, 'experiment', 'nopuncta_map.xlsx')
+        self.pfam_puncta = os.path.join(data_dp, 'experiment', 'puncta_pfam.tsv')
+        self.pfam_nopuncta = os.path.join(data_dp, 'experiment', 'nopuncta_pfam.tsv')
+
+    def read_file(self):
+        """
+        uniprot_acc
+        pfamA_acc
+        seq_start
+        seq_end
+        """
+        unis = self.get_nopuncta_uni()
+        sdf = pd.DataFrame()
+        for i, chunk in enumerate(pd.read_csv(self.pfam_fp, sep='\t', chunksize=100000)):
+            print(i)
+            ndf = chunk[chunk['uniprot_acc'].isin(unis)]
+            ndf = ndf[['uniprot_acc', 'pfamA_acc', 'seq_start', 'seq_end']]
+            sdf = pd.concat([sdf, ndf])
+        sdf.to_csv(self.pfam_nopuncta, sep='\t')
+
+    def get_puncta_uni(self):
+        df = pd.read_excel(self.puncta_map, sheetname='puncta_map')
+        unis = list(df['Uni_ID'])
+        return unis
+
+    def get_nopuncta_uni(self):
+        df = pd.read_excel(self.nopuncta_map_excel, sheetname='nopuncta_map')
+        unis = list(df['Uni_ID'])
+        return unis
+
+    def write_nopuncta_map(self):
+        puncta = pd.read_excel(self.puncta, sheetname='NoPuncta')
+        no_puncta_orfs = list(puncta['ORF'])
+        df = pd.read_excel(self.sgd_uni_fp, sep='\t')
+        df = df[df['ORF'].isin(no_puncta_orfs)]
+        df.to_csv(self.nopuncta_map, sep='\t')
+
+
+def main():
+    p = Pfam()
+    p.read_file()
+
+
+if __name__ == '__main__':
+    main()
